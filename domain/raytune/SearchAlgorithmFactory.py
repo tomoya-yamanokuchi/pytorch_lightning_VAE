dba84e733b894c5def2b5543f137ec0207067dd7
tomoya-y <yamanokuchi.tomoya.ys9@is.naist.jp>
2022-09-22 10:53:32 +0900

    ray tune のバージョンが2.0.0にアップしていてそれに合わせて修正

--- domain/raytune/SearchAlgorithmFactory.py
+++ domain/raytune/SearchAlgorithmFactory.py
@@ -1,10 +1,9 @@
-from ray.tune import suggest
-from ray.tune.suggest import ConcurrencyLimiter
+from ray.tune import search
+from ray.tune.search import ConcurrencyLimiter
 
 class SearchAlgorithmFactory:
     def create(self, name: str, max_concurrent: int, **kwargs):
-        search_alg = suggest.SEARCH_ALG_IMPORT[name]()(**kwargs)
+        search_alg = search.SEARCH_ALG_IMPORT[name]()(**kwargs)
         search_alg = ConcurrencyLimiter(search_alg, max_concurrent=max_concurrent)
-        # search_alg = suggest.SEARCH_ALG_IMPORT[name]()(**kwargs)
         return search_alg
 
